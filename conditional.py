421f142975864d45233519bf0f98b1faf95b4197
Jennifer Vezilj <jennifervezilj@dhcp-10-132-148-221.uawifi.arizona.edu>
2016-01-30 16:22:25 -0700

    initial commit

---
+++ conditional.py
@@ -0,0 +1,6 @@
+x = 10
+
+if x < 15:
+    print("Need more cats")
+else:
+    print("Cat lady")
---
+++ iteration.py
@@ -0,0 +1,5 @@
+books = ["Tale of Two Cities", "Harry Potter", "Lord of the Rings"]
+nums = range(100)
+
+for b in books:
+    print (b)
